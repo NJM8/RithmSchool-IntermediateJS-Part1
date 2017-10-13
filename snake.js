6202265d680decd84722b9bce78b63bbbb5b21ee
Nate Mallison <healthynate@gmail.com>
2017-10-12 22:07:13 -0400

    got directional input working with filter so you cant go opposite directions. history implemented for drawing the tail of the snake, length can be easily adjusted.

--- snake.js
+++ snake.js
@@ -3,6 +3,9 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var width = 0;
 var height = 0;
+var currentDirection = '';
+var stateHistory = [];
+var length = 10;
 
 var reSize = function(){
 	width = window.innerWidth * 2;
@@ -27,21 +30,78 @@ var state = {
 	}
 }
 
+var keyMap = {
+	37: 'left', 
+	39: 'right',
+	38: 'up',
+	40: 'down'
+}
+
+var oppositeDirections = {
+	'left': 'right', 
+	'right': 'left',
+	'up': 'down',
+	'down': 'up'
+}
+
+function keyDown(event){
+	var key = keyMap[event.keyCode];
+	if (oppositeDirections[key] === currentDirection) {
+		return;
+	}
+	for (value in state.pressedKeys) {
+		if (state.pressedKeys[value]){
+			state.pressedKeys[value] = false;
+		}
+	}
+	state.pressedKeys[key] = true;
+	currentDirection = key;
+}
+
+window.addEventListener('keydown', keyDown, false);
+
 function update(progress){
-	state.x += progress;
+	if (state.pressedKeys.left) {
+		state.x -= progress;
+	}
+
+	if (state.pressedKeys.right) {
+		state.x += progress;
+	}
+
+	if (state.pressedKeys.up) {
+		state.y -= progress;
+	}
+
+	if (state.pressedKeys.down) {
+		state.y += progress;
+	}
 
 	if (state.x > width) {
 		state.x -= width;
+	} else if (state.x < 0) {
+		state.x += width;
+	}
+
+	if (state.y > height) {
+		state.y -= height;
+	} else if (state.y < 0) {
+		state.y += height;
 	}
 }
 
 function draw(){
 	ctx.clearRect(0, 0, width, height);
-	ctx.fillRect(state.x - 5, state.y - 5, 12, 12);
+
+	stateHistory.unshift([state.x, state.y]);
+	stateHistory.length = 200;
+	stateHistory.forEach(function(element){
+		ctx.fillRect(element[0] - 5, element[1] - 5, 12, 12);
+	})
 }
 
 function loop(timeStamp){
-	var progress = (timeStamp - lastRender) / 3.5;
+	var progress = (timeStamp - lastRender) / 3;
 
 	update(progress);
 	draw();
